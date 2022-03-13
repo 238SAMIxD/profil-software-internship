62d261fbfdedb733abc007827480c4eb040bc981
238SAMIxD <samijedrzejewski@gmail.com>
2022-03-13 18:00:32 +0100

    minor typo fixed
    Fixed some code typos.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const open = require('open');
 
 const index = "./public/index.html";
 const app = express();
+
 const host = "localhost";
 const port = 3000;
 
@@ -11,5 +12,5 @@ app.use( express.static( path.join( __dirname, "public" ) ) );
 
 app.listen( port, host, () => {
     console.log(`App running on ${host}:${port}/`);
-    open(`http://${host}:${port}` );
+    open(`http://${host}:${port}`);
 });
